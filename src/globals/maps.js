5bc2c163779d1fd64fab98329cda923bf377f8f3
Yehuda B <yehudab@gmail.com>
2022-12-02 14:23:34 +0200

    New map pages for each trail segment

---
+++ src/globals/maps.js
@@ -0,0 +1,24 @@
+const fs = require("fs").promises;
+const glob = require("glob-promise");
+const topojson = require("topojson-client");
+
+module.exports = async function() {
+    const allTrails = await glob('public/topojson/trail-*.topojson');
+    return Promise.all(
+        allTrails.map(parseMapFile)
+    )
+};
+
+async function parseMapFile(file) {
+    let objectName = file.replace(/.*(trail-[0-9]+).*/, "$1");
+    let number = parseInt(objectName.replace("trail-", ""), 10);
+    const data = await fs.readFile(file, 'utf-8');
+    const topology = JSON.parse(data);
+    let geojson = topojson.feature(topology, topology.objects[objectName]);
+    let geojsonFeature = geojson.features[0];
+    let name = geojsonFeature.properties.name;
+    geojsonFeature = JSON.stringify(geojsonFeature);
+
+    return {number, name, geojsonFeature};
+}
+
