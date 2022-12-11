6f6697696b7875df2a3ea2effe0b946b8bc6785a
yehudab <yehudab@users.noreply.github.com>
2022-12-11 20:02:14 +0000

    deploy: 1671e8cf3b448dd9b1a55eaafaeb669e5c85b687

--- img/2018/xss.js
+++
@@ -1,2 +0,0 @@
-$('.no-products span').text('רק היום 60% הנחה. לקבלת קופון היכנסו לאזור האישי מהקישור בפינה הימנית עליונה ')
-$('input[name=Authentication]').attr('placeholder', 'מספר כרטיס אשראי').on('change', function(){console.log('credit card number is: ', $(this).val())}) 
