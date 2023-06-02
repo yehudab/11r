c078af0bd852052acfc08e70066538fbb9bfb2ea
Yehuda B <yehudab@gmail.com>
2023-06-02 12:44:39 +0300

    upgrade to latest npm packages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import postcss from 'rollup-plugin-postcss';
 import replace from '@rollup/plugin-replace';
 import svg from 'rollup-plugin-svg';
 import path from 'path'
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const dev = process.env.NODE_ENV !== 'production';
 
